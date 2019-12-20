8e6b8b1e3b9139cafd88163d9622e5d6168c9a2f
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 21:53:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,11 @@ array = (1..num).to_a
  array.each do |d|
    if array.select { |d| num % d == 0 } == [1, num]
       return true 
-    else num < 2
+    else
       false
    end
+end
+if num < 2 
+  return false
 end
 end
