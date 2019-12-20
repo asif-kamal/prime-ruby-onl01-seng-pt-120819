663424d93b7d269fb139758af7b1eafdb0518308
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 21:50:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+
+array = (1..num).to_a
+
+ array.each do |d|
+   if array.select { |d| num % d == 0 } == [1, num]
+      return true 
+    else 
+      false
+   end
+end
+end
